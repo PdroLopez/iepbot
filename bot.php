bb18309f2c965c90c9572c22d6a04d595c7fe9be
PdroLopez <114783465+PdroLopez@users.noreply.github.com>
2025-08-29 18:36:02 -0400

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,32 @@
+<?php
+
+$token = '8368397544:AAFQcS9a7SiHJ8wDzxWMPqs75CnaLL6aHtI';
+$website = 'https://api.telegram.org/bot'.$token;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+switch($message)
+{
+    case '/start':
+		$response = 'ddd';
+        sendMessage($chatId,$response);
+        break;
+
+    case 'como me ira':
+        $response ='bien po';
+        sendMessage($chatId,$response);
+        break;
+    default:
+        $response = 'no entendi jaja';
+        sendMessage($chatID,$response);
+
+
+}
+
+
+
+?>
