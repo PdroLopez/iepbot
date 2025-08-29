01533350b47e9c12d86bf2186c8885e97a7a8f26
PdroLopez <114783465+PdroLopez@users.noreply.github.com>
2025-08-29 18:45:26 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,21 +1,16 @@
 <?php
-
 $token = '8368397544:AAFQcS9a7SiHJ8wDzxWMPqs75CnaLL6aHtI';
 $website = 'https://api.telegram.org/bot'.$token;
-
 $input = file_get_contents('php://input');
 $update = json_decode($input, TRUE);
-
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
-
 switch($message)
 {
     case '/start':
 		$response = 'ddd';
         sendMessage($chatId,$response);
         break;
-
     case 'como me ira':
         $response ='bien po';
         sendMessage($chatId,$response);
@@ -23,10 +18,5 @@ switch($message)
     default:
         $response = 'no entendi jaja';
         sendMessage($chatID,$response);
-
-
 }
-
-
-
 ?>
